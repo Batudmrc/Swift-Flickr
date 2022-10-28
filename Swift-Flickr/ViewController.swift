aaf8354f06a9dbf52693cd9172177493fab254de
Batuhan <bthn_chn_dmrc@hotmail.com>
2022-10-28 17:56:54 +0300

    Initial Commit

---
+++ Swift-Flickr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-Flickr
+//
+//  Created by Batuhan Demircioğlu on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
